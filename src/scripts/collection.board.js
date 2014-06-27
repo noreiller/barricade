66380413893695d114fa63b378e83ba533416a54
Aurélien MANCA <aurelien@amanca.fr>
2014-06-27 08:14:22 +0200

    Avoid loops on an empty collection

--- src/scripts/collection.board.js
+++ src/scripts/collection.board.js
@@ -12,25 +12,27 @@ define([
 		, getGrid: function () {
 			var grid = [];
 
-			var rows = _.reduce(this.models, function (prev, current) {
-				return current.get('row') > prev.get('row') ? current : prev;
-			}).get('row');
+			if (this.length) {
+				var rows = _.reduce(this.models, function (prev, current) {
+					return current.get('row') > prev.get('row') ? current : prev;
+				}).get('row');
 
-			var cols = _.reduce(this.models, function (prev, current) {
-				return current.get('col') > prev.get('col') ? current : prev;
-			}).get('col');
+				var cols = _.reduce(this.models, function (prev, current) {
+					return current.get('col') > prev.get('col') ? current : prev;
+				}).get('col');
 
-			for (var row = 0; row <= rows; row++) {
-				grid.push([]);
+				for (var row = 0; row <= rows; row++) {
+					grid.push([]);
 
-				for (var col = 0; col <= cols; col++) {
-					grid[row][col] = 0;
+					for (var col = 0; col <= cols; col++) {
+						grid[row][col] = 0;
+					}
 				}
-			}
 
-			_.each(this.models, function (item, i) {
-				grid[item.get('row')][item.get('col')] = item.get('value');
-			}, this);
+				_.each(this.models, function (item, i) {
+					grid[item.get('row')][item.get('col')] = item.get('value');
+				}, this);
+			}
 
 			return grid;
 		}
