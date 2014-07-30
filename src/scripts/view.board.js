e8faa2e9d5702e4722c2e8c3517ac6e29c2c8a72
Aurélien MANCA <aurelien@amanca.fr>
2014-07-30 16:35:51 +0200

    Fixed the update of the player settings

--- src/scripts/view.board.js
+++ src/scripts/view.board.js
@@ -3,69 +3,13 @@
 	define([
 		'underscore'
 		, 'backbone'
-		, 'tools'
-		, 'game.events'
-		, 'game.user'
-		, 'view.abstract'
-		, 'view.place'
-	], function (_, Backbone, Tools, Events, User, AbstractView, PlaceView) {
+		, 'view.board.2d'
+		, 'view.board.dom'
+	], function (_, Backbone, Board2dView, BoardDomView) {
 		'use strict';
 
-		var View = AbstractView.extend({
-			initialize: function (options) {
-				_.bindAll(this
-					, 'render'
-					, 'renderItem'
-					, 'updateViewport'
-				);
-
-				this.views = new Backbone.ChildViewContainer();
-
-				this.listenTo(this.collection, 'reset', this.render);
-				this.listenTo(this.collection, 'add', this.renderItem);
-				this.listenTo(Events, 'window:resize', this.updateViewport);
-
-				this.render();
-
-				return this;
-			}
-
-			, render: function () {
-				this.empty();
-
-				_.each(this.collection.models, this.renderItem, this);
-
-				return this;
-			}
-
-			, renderItem: function (item) {
-				var itemView = new PlaceView({
-					model: item
-					, parentNode: this.el
-				});
-
-				this.views.add(itemView, item.cid);
-
-				return this;
-			}
-
-			, updateViewport: function () {
-				this.el.title = User.storage.get('mapColCount') + 'x' + User.storage.get('mapRowCount');
-
-				if (!User.storage.get('mapRowCount') || !User.storage.get('mapColCount')) {
-					this.hide();
-				}
-				else {
-					this.show();
-
-					this.views.apply('updateViewport');
-				}
-
-				return this;
-			}
-		});
-
-		return View;
+		// return BoardDomView;
+		return Board2dView;
 	});
 
 })(window, document);
